ba22012e386019f1161d79c306124abd1652224e
xiaolu991954992 <991954992@qq.com>
2018-12-27 09:35:43 +0800

    服务网关zuul,高级应用，filter,fallback,retry

---
+++ gateway-service-zuul-2/src/main/java/cn/rankey/springcloud/ZuulApplication.java
@@ -0,0 +1,20 @@
+package cn.rankey.springcloud;
+
+import cn.rankey.springcloud.filter.TokenFilter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ZuulApplication.class,args);
+    }
+
+    @Bean
+    public TokenFilter tokenFilter(){
+        return new TokenFilter();
+    }
+}
