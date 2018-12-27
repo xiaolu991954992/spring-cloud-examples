7f0c9208f47457187f6a9d52b25f77bee07cd8dd
xiaolu991954992 <991954992@qq.com>
2018-12-27 22:58:19 +0800

    服务链路追踪

--- gateway-service-zuul/src/main/java/cn/rankey/springcloud/ZuulApplication.java
+++ gateway-service-zuul/src/main/java/cn/rankey/springcloud/ZuulApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableZuulProxy
 public class ZuulApplication {
+
     public static void main(String[] args){
         SpringApplication.run(ZuulApplication.class,args);
     }
